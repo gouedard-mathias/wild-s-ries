8457508baa40380666fdd34b5c621160358637fe
gouedard-mathias <m-gouedard40500@hotmail.fr>
2020-12-01 07:43:42 +0100

    05 - Symfony : Le routing avancé

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,8 +4,9 @@
 namespace App\Controller;
 
 
-use Symfony\Component\HttpFoundation\Response;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
